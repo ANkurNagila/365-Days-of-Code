86148edb6fac502ffa93c6d81aa691557ad29a3b
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2021-03-18 16:54:58 +0530

    question 1(code chef)

---
+++ Code1/hidden_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(void) {
+	int T;
+	int N,flag;
+	scanf("%d",&T);
+	for(int i=1;i<=T;i++)
+	{
+	    flag=0;
+	    scanf("%d",&N);
+	    for(int j=2;j<sqrt(N);j++)
+	    {
+	        if(N%j==0)
+	        {
+	            printf("%d %d\n",j,N/j);
+	            flag=1;
+	            break;
+	        }
+	    }
+	    if(flag==0)
+	    {
+	        printf("1 %d\n",N);
+	    }
+	}
+	return 0;
+}
