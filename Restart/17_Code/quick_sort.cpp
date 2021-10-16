e8ca30eedd67d12864066ebd0851e15207b18c14
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2021-10-16 11:00:05 +0530

    Update quick_sort.cpp

--- Restart/17_Code/quick_sort.cpp
+++ Restart/17_Code/quick_sort.cpp
@@ -2,76 +2,95 @@
 using namespace std;
 
 
-int partition(int *input,int size)
+int partition(int input[],int size,int pos=0)
 {
-    int count=0;
+    int cou=0;
     for(int i=1;i<size;i++)
     {
-        if(input[i]<input[0])
-            count++;
+        if(*(input+i)<(*(input)))
+        {
+            cou++;
+        }
     }
-
-    return count;
+    return cou;
 }
 
 
 
-void quickSort(int input[], int size) {
-
+void quickSort(int input[], int size)
+{
     if(size<=1)
-        return ;
+        return;
 
     int index=partition(input,size);
-    cout<<input[index]<<endl;
 
 
-    int temp=input[0];
-    input[0]=input[index];
-    input[index]=temp;
-
-
-    for(int i=0,j=index+1;i<index and j<size;)
+    if(index==0)
     {
-        if(input[i]<input[index] and input[j]>=input[index])
+        quickSort(input+1,size-1);
+    }
+    else
+    {
+        int temp=input[0];
+        input[0]=input[index];
+        input[index]=temp;
+
+        int input1[index],pivot=input[index],input2[size-index-1];
+        int k=0;
+        for(int i=0;i<index;i++)
         {
-            i++;
-            j++;
+            input1[i]=input[k];
+            k++;
         }
-        else if(input[i]<input[index])
-            i++;
-        else if(input[j]>=input[index])
-            j++;
-        else
+
+        k++;
+        for(int j=0;j<(size-index-1);j++)
         {
-            temp=input[i];
-            input[i]=input[j];
-            input[j]=temp;
-            i++;
-            j++;
+            input2[j]=input[k];
+            k++;
+        }
+
+
+        int i=0,j=0;
+        while(i<index and j<(size-index-1))
+        {
+            if(input1[i]>=input[index] and input2[j]<input[index])
+            {
+                int temp1=input1[i];
+                input1[i]=input2[j];
+                input2[j]=temp1;
+                i++;
+                j++;
+            }
+            else if(input1[i]<input[index])
+            {
+                i++;
+            }
+            else if(input2[j]>=input[index])
+            {
+                j++;
+            }
+        }
+
+        quickSort(input1,index);
+        quickSort(input2,size-index-1);
+
+        k=0;
+        for(int i=0;i<index;i++)
+        {
+            input[k]=input1[i];
+            k++;
+        }
+
+        k++;
+        for(int j=0;j<(size-index-1);j++)
+        {
+            input[k]=input2[j];
+            k++;
         }
     }
-
-    int input1[index];
-    int k=0;
-    for(int i=0;i<index;i++)
-    {
-        input1[i]=input[k];
-        k++;
-    }
-    k++;
-
-    int input2[size-index-1];
-    for(int i=0;i<size-index-1;i++)
-    {
-        input2[i]=input[k];
-        k++;
-    }
-
-
-    quickSort(input1,index+1);
-    quickSort(input2,size-index+1);
-
 }
+
 
 int main(){
     int n;
