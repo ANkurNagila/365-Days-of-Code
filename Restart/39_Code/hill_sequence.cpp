681e7236f9448b20a861da2b8c0eb168cbdd1b8b
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2021-11-06 23:32:45 +0530

    Create hill_sequence.cpp

---
+++ Restart/39_Code/hill_sequence.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+	int t;
+	cin>>t;
+
+	while(t--)
+    {
+        int n;
+        cin>>n;
+
+        vector <long long int> v1;
+        vector <long long int> uncommon;
+        vector <long long int> common;
+
+        int flag=0;
+
+        for(int i=0;i<n;i++)
+        {
+            long long int test;
+            cin>>test;
+
+            v1.push_back(test);
+        }
+        sort(v1.begin(),v1.end(),greater<int>());
+
+        for(int i=0;i<n;i++)
+        {
+            if(v1[i-1]==v1[i] and v1[i]!=v1[i+1])
+            {
+                common.push_back(v1[i]);
+            }
+            else if(v1[i-1]==v1[i] and (v1[i]==v1[i+1] or i==n-1))
+            {
+                flag=1;
+            }
+            else
+            {
+                uncommon.push_back(v1[i]);
+            }
+        }
+
+        if(flag==0)
+        {
+            for(int i=common.size()-1;i>=0;i--)
+                cout<<common[i]<<" ";
+
+            for(int i=0;i<uncommon.size();i++)
+                cout<<uncommon[i]<<" ";
+
+        }
+        else
+        {
+            cout<<"-1";
+        }
+        cout<<endl;
+    }
+
+	return 0;
+}
+
